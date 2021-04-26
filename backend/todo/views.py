d5a80c90bf72993a8b0a97c6aaa4a1bbf5fe0c89
Baribor Saturday <saturdaybaribor@gmail.com>
2021-04-26 03:00:36 +0100

    added a commit in index

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    """
+    returns the index page for the url todo/
+    """
     return render(request, 'todo/index.html',{'message':'Welcome to the index page'})
