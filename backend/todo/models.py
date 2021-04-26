01e495e0d545f916ef490eb934e44991350371be
Baribor Saturday <saturdaybaribor@gmail.com>
2021-04-26 02:53:46 +0100

    created first view "index.hml"

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.fields import CharField, TextField
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = CharField(max_length=120)
+    description = TextField()
+
+    class Meta:
+        verbose_name = "Todo"
+        verbose_name_plural = "Todos"
+
+    def __str__(self):
+        return self.title
