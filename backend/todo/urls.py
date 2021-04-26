912af4d24557419ca434e2f1e0048236bdd10347
Baribor Saturday <saturdaybaribor@gmail.com>
2021-04-26 03:32:59 +0100

    added a new view

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -5,5 +5,6 @@ app_name = 'todo'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('create/', views.create_todo, name="create_todo"),
 ]
  
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -7,3 +7,7 @@ def index(request):
     returns the index page for the url todo/
     """
     return render(request, 'todo/index.html',{'message':'Welcome to the index page'})
+
+def create_todo(request):
+    "return the create todo page"
+    return render(request, 'todo/create.html',{'message':"How do you want to create your todo"})
